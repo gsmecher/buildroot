6157c8fa59c4a1d3bc2b1dc353ebae91d97a1afd
Graeme Smecher <gsmecher@threespeedlogic.com>
2022-04-07 10:59:48 -0700

    Tuber v0.3: removes nlohmann::json and pybind11_json dependencies.

--- package/tuber/tuber.mk
+++ package/tuber/tuber.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TUBER_VERSION = HEAD
+TUBER_VERSION = v0.3
 TUBER_SITE_METHOD = git
 TUBER_SITE = ssh://git@github.com/gsmecher/tuber.git
 TUBER_LICENSE = GPL-3.0
@@ -13,6 +13,6 @@ TUBER_SUPPORTS_IN_SOURCE_BUILD = YES
 
 TUBER_CONF_OPTS = -DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python3
 
-TUBER_DEPENDENCIES = python-pybind boost libhttpserver fmt python-pybind-json
+TUBER_DEPENDENCIES = python-pybind boost libhttpserver fmt
 
 $(eval $(cmake-package))
