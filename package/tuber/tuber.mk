6e5ed697a832f8827034ba1725611087949c453c
Graeme Smecher <gsmecher@threespeedlogic.com>
2022-05-11 13:34:41 -0700

    package/tuberd: bump to 0.5 (now includes enum support header.)

--- package/tuber/tuber.mk
+++ package/tuber/tuber.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-TUBER_VERSION = v0.4
+TUBER_VERSION = v0.5
 TUBER_SITE_METHOD = git
 TUBER_SITE = ssh://git@github.com/gsmecher/tuber.git
 TUBER_LICENSE = GPL-3.0
 TUBER_LICENSE_FILES = LICENSE
 TUBER_SUPPORTS_IN_SOURCE_BUILD = YES
+TUBER_INSTALL_STAGING = YES
 
 TUBER_CONF_OPTS = -DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python3
 
